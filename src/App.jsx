bc924eae789bdef8938bf8921c1ca5f9179ca17c
ChristelDeveza <chris.dvz66@gmail.com>
2022-07-02 09:51:06 +0200

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
+import About from './Components/About'
+import Projects from './Components/Projects'
+import Contact from './Components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +12,12 @@ function App() {
   return (
     <div className="App">
       hello
+
+      <Navbar />
+      <About />
+      <Projects />
+      <Contact />
+
     </div>
   )
 }

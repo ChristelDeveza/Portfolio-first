38aa7006481b8dd18920c21bcf3a44a73eecf12d
ChristelDeveza <chris.dvz66@gmail.com>
2022-07-03 16:49:40 +0200

    cube and cercles animations

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import picture1 from "../Img/left-and-right-arrows.svg";
-import picture2 from "../Img/Binary-Globe.svg";
-import "./Home.css";
-
+import React from 'react';
+import picture1 from '../Img/left-and-right-arrows.svg';
+import picture2 from '../Img/Binary-Globe.svg';
+import './Home.css';
 
 function Home() {
   return (
-    <div className='home' id="Accueil">
+    <div className='home' id='Accueil'>
       <div className='home-container'>
         <h1 className='home-text'>Bonjour et bienvenue sur mon portfolio</h1>
-       </div>
-       <img className='home-img' src={picture1} alt="picture"/>
-       <img className='home-img2' src={picture2} alt="picture2"/>
-
+      </div>
+      <div class='wrap'>
+        <div class='cube'>
+          <div class='front'>front</div>
+          <div class='back'>back</div>
+          <div class='top'>top</div>
+          <div class='bottom'>bottom</div>
+          <div class='left'>left</div>
+          <div class='right'>right</div>
+        </div>
+      </div>
+      <div className='ring-container'>
+        <div className='ring'></div>
+        <div className='ring'></div>
+        <div className='ring'></div>
+      </div>
+      <div>
+        <img className='home-img2' src={picture2} alt='picture2' />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

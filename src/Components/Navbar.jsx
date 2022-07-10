5f368e9f25a15600f526630fe4c1f4e85c678700
ChristelDeveza <chris.dvz66@gmail.com>
2022-07-10 15:24:14 +0200

    Navbar responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 function Navbar() {
+  const [sidebar, setSidebar]=useState(false)
+
+  function showSidebar(){
+    setSidebar(!sidebar)
+  }
   return (
     <div className='Navbar'>
       <nav>
         <ul>
-          <div className='"main-nav'>
+          <div className='button-container'>
             <button
-              className='hamburger open'
+              className={sidebar?'hamburger open':'hamburger'}
               type='button'
               aria-label='Toggle nav'
               aria-expanded='false'
+              onClick={showSidebar}
             >
               <span></span>
               <span></span>
               <span></span>
             </button>
           </div>
-          <div className='nav-container'>
-          <li>
-            <a href='/'>Accueil</a>
-          </li>
-          <li>
-            <a href='/#About'>A propos</a>
-          </li>
-          <li>
-            <a href='/#Projects'>Projets</a>
-          </li>
-          <li>
-            <a href='/#Skills'>Compétences</a>
-          </li>
-          <li>
-            <a href='/#Contact'>Contact</a>
-          </li>
+          <div className={!sidebar? 'nav-container':'nav-container open'}>
+            <li>
+              <a href='/'>Accueil</a>
+            </li>
+            <li>
+              <a href='/#About'>A propos</a>
+            </li>
+            <li>
+              <a href='/#Projects'>Projets</a>
+            </li>
+            <li>
+              <a href='/#Skills'>Compétences</a>
+            </li>
+            <li>
+              <a href='/#Contact'>Contact</a>
+            </li>
           </div>
         </ul>
       </nav>
